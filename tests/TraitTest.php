3c3f8dc4c6a72506070a04384180d94025e6f089
ChurakovMike <churakovmike@mail.ru>
2020-04-12 23:45:11 +0700

    + add some test

--- tests/TraitTest.php
+++ tests/TraitTest.php
@@ -9,8 +9,34 @@ class TraitTest extends TestCase
 {
     public function test_success_cast_type_setting()
     {
-        $mock = $this->getMockForTrait(TypeCastTrait::class);
+        $mock = $this->getMockTrait();
         $mock->setCasts([]);
         $this->assertIsArray($mock->getCasts());
+        $this->assertEmpty($mock->getCasts());
+    }
+
+    public function test_success_check_casts()
+    {
+        $mock = $this->getMockTrait();
+        $mock->setCasts([
+            'test' => 'integer',
+        ]);
+        $this->assertIsArray($mock->getCasts());
+        $this->assertArrayHasKey('test', $mock->getCasts());
+
+    }
+
+    public function test_check_has_casts_method()
+    {
+        $mock = $this->getMockTrait();
+        $mock->setCasts([
+            'test' => 'integer',
+        ]);
+        $this->assertTrue($mock->hasCast('test'));
+    }
+
+    protected function getMockTrait()
+    {
+        return $this->getMockForTrait(TypeCastTrait::class);
     }
 }
